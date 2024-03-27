a22dd8953b019c8a1132dc2c794d92b242a3cf95
AQSA FIRDOUS <zifrafirdous@gmail.com>
2024-03-27 13:22:03 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+// computer generate a random number
+
+// user input for guessing number
+
+// compare user input with computer generated number and show result
+
+
+const randomNumber = Math.floor(Math.random() * 5 + 1)
+
+const answers = await inquirer.prompt([
+    {
+        name : "userGuessedNumber",
+        type : "number" ,
+        message : "Please guess a number between 1-5:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber){
+    console.log("Congratulation ! You guess right number." );
+} else {
+    console.log( "You guess wrong number" );
+}
